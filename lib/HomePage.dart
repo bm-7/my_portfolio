58d6df40cceda697afaa8d848ce06505e5b23082
food_ui <mohithparivar@gmail.com>
2024-05-02 10:41:41 +0530

    aboutMe.dart

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,11 +1,8 @@
 import 'dart:async';
 
-import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
-import 'package:my_portfolio/aboutMe.dart';
+import 'aboutMe.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -41,193 +38,127 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Animated Horizontal Scrolling Background'),
-      // ),
-      body: AnimationConfiguration.synchronized(
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Stack(
-            children: [
-              Container(
-                height: 3000,
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(20)),
-                child: Stack(
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Stack(
+          children: [
+            Container(
+              height: 3000,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: _offset,
+                    child: AnimatedContainer(
+                      color: Colors.black,
+                      duration: const Duration(milliseconds: 100),
+                      width: MediaQuery.of(context).size.width * 2,
+                      height: 3000,
+                      child: _buildBackgroundContent(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              left: -1,
+              top: -35,
+              child: Container(
+                width: 300,
+                height: 260,
+                child: Image.asset(
+                  "Assets/111.gif",
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            AnimatedPositioned(
+              duration: const Duration(milliseconds: 1000),
+              left: _isVisible ? 40 : -300,
+              top: 150,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                width: _isVisible ? 1500 : 100,
+                child: Row(
                   children: [
-                    Positioned(
-                      left: _offset,
-                      child: AnimatedContainer(
-                        color: Colors.black,
-                        duration: const Duration(milliseconds: 100),
-                        width: MediaQuery.of(context).size.width * 2,
-                        height: 3000,
-                        child: _buildBackgroundContent(),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Hello, I'm",
+                          style: TextStyle(fontSize: 32, color: Colors.white54),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Mohith B M",
+                          style: TextStyle(
+                              fontSize: 36,
+                              color: Color.fromARGB(255, 77, 193, 255)),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Flutter Developer",
+                          style: TextStyle(fontSize: 36, color: Colors.white),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Text(
+                              "Building Apps  |",
+                              style: TextStyle(
+                                  fontSize: 20, color: Colors.white60),
+                            ),
+                            Text(
+                              "   Mohith, Flutter Dev",
+                              style: TextStyle(
+                                  fontSize: 20, color: Colors.white60),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(width: 450),
+                    Container(
+                      height: 400,
+                      width: 400,
+                      child: Stack(
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.blue,
+                            radius: 200,
+                          ),
+                          Center(
+                            child: CircleAvatar(
+                              radius: 180,
+                              backgroundImage: AssetImage("Assets/2.jpg"),
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 1,
+                            child: Container(
+                              height: 100,
+                              width: 100,
+                              decoration: FlutterLogoDecoration(),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
               ),
-              Positioned(
-                left: -1,
-                top: -35,
-                child: Container(
-                  width: 300,
-                  height: 260,
-                  child: Image.asset(
-                    "Assets/111.gif",
-                    fit: BoxFit.fill,
-                  ),
-                ),
+            ),
+            Positioned(
+              top: 600,
+              left: 0, // Adjust the left position to align properly
+              child: Container(
+                width: MediaQuery.of(context).size.width, // Make the container full width
+                color: Colors.amber,
+                padding: const EdgeInsets.all(20),
+                child: AboutMe(), // Use the AboutMe widget here
               ),
-              AnimatedPositioned(
-                duration: const Duration(milliseconds: 1000),
-                left: _isVisible ? 40 : -300,
-                top: 180,
-                child: AnimatedContainer(
-                  duration: const Duration(milliseconds: 500),
-                  width: _isVisible ? 1500 : 100,
-                  child: Row(
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          AnimatedTextKit(
-                            isRepeatingAnimation: false,
-                            animatedTexts: [
-                              TypewriterAnimatedText(
-                                "Hello, I'm",
-                                textStyle: const TextStyle(
-                                  fontSize: 37,
-                                  color: Colors.white54,
-                                ),
-                                speed: const Duration(milliseconds: 100),
-                              ),
-                            ],
-                          ),
-                          AnimatedTextKit(
-                              isRepeatingAnimation: false,
-                              animatedTexts: [
-                                TypewriterAnimatedText(
-                                  "Mohith B M",
-                                  textStyle: const TextStyle(
-                                    fontSize: 42,
-                                    color: Color.fromARGB(255, 77, 193, 255),
-                                  ),
-                                  speed: const Duration(milliseconds: 500),
-                                ),
-                              ]),
-                          AnimatedTextKit(
-                              isRepeatingAnimation: false,
-                              animatedTexts: [
-                                TypewriterAnimatedText(
-                                  "Flutter Developer",
-                                  textStyle: const TextStyle(
-                                    fontSize: 42,
-                                    color: Colors.white,
-                                  ),
-                                  speed: const Duration(milliseconds: 500),
-                                ),
-                              ]),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            children: [
-                              AnimatedTextKit(
-                                  isRepeatingAnimation: false,
-                                  animatedTexts: [
-                                    TypewriterAnimatedText(
-                                      "Building Apps  |",
-                                      textStyle: const TextStyle(
-                                          fontSize: 22, color: Colors.white60),
-                                    ),
-                                  ]),
-                              AnimatedTextKit(
-                                  isRepeatingAnimation: false,
-                                  animatedTexts: [
-                                    TypewriterAnimatedText(
-                                      " Mohith, Flutter Dev",
-                                      textStyle: const TextStyle(
-                                          fontSize: 22, color: Colors.white60),
-                                    )
-                                  ])
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 50,
-                          ),
-                          FadeInAnimation(
-                            duration: Duration(milliseconds: 3000),
-                            curve: Curves.linear,
-                            child: FilledButton(
-                              onPressed: () {},
-                              child: const Text(
-                                "Download Resume >",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w500, fontSize: 16),
-                              ),
-                              style: const ButtonStyle(
-                                backgroundColor: MaterialStatePropertyAll(
-                                    Color.fromARGB(255, 77, 193, 255)),
-                                shape: MaterialStatePropertyAll(
-                                    BeveledRectangleBorder()),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        width: 350,
-                      ),
-                      FlipAnimation(
-                        duration: const Duration(milliseconds: 3000),
-                        curve: Curves.decelerate,
-                        child: Container(
-                          height: 400,
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.blue,
-                          ),
-                          width: 400,
-                          child: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Stack(
-                              children: [
-                                const CircleAvatar(
-                                  backgroundColor: Colors.black,
-                                  radius: 200,
-                                ),
-                                const Center(
-                                  child: CircleAvatar(
-                                    radius: 180,
-                                    backgroundImage: AssetImage("Assets/2.jpg"),
-                                  ),
-                                ),
-                                Positioned(
-                                  bottom: 1,
-                                  child: Container(
-                                    height: 100,
-                                    width: 100,
-                                    decoration: const FlutterLogoDecoration(),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 500,
-                height: 800,
-                child: AboutMe(),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
@@ -237,7 +168,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Row(
       children: List.generate(
         450,
-        (index) => Row(
+            (index) => Row(
           children: [
             Container(
               width: 150,
--- lib/aboutMe.dart
+++ lib/aboutMe.dart
@@ -7,151 +7,144 @@ class AboutMe extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-        color: Colors.black,
-        padding: const EdgeInsets.all(20),
-        child: Column(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          "About Me :",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 25),
+        Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              "About Me :",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Crafting excellence in Flutter development",
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: Color.fromARGB(255, 77, 193, 255),
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  Text(
+                    "I'm Mohith B M, a mobile app developer, particularly in Flutter. I'm always looking to learn and improve. More than just coding, I believe in never giving up, a trait that fuels my passion for building innovative and user-friendly mobile experiences.",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white60,
+                    ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 25),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+            const SizedBox(width: 20),
+            FadeInAnimation(
+                duration: Duration(milliseconds: 3000),
+                curve: Curves.linear,
+                child: Container(
+                  width: 700,
+                  color: Colors.black,
+                  child: Stack(
                     children: [
-                      Text(
-                        "Crafting excellence in Flutter development",
-                        style: TextStyle(
-                          fontSize: 26,
-                          color: Color.fromARGB(255, 77, 193, 255),
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 90),
+                          child: Container(
+                            width: 300,
+                            height: 400,
+                            color: Colors.amber,
+                            child: const Image(
+                              fit: BoxFit.cover,
+                              image: AssetImage("Assets/3.jpg"),
+                            ),
+                          ),
                         ),
                       ),
-                      SizedBox(height: 40),
-                      Text(
-                        "I'm Mohith B M, a mobile app developer, particularly in Flutter. I'm always looking to learn and improve. More than just coding, I believe in never giving up, a trait that fuels my passion for building innovative and user-friendly mobile experiences.",
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white60,
+                      Positioned(
+                        top: 200,
+                        left: 50,
+                        child: Container(
+                          width: 260,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Icon(
+                                  Icons.developer_mode_outlined,
+                                  color: Colors.blue,
+                                ),
+                                Text(
+                                  "Application Developer",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.call_made_outlined,
+                                  color: Colors.black,
+                                  size: 30,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 300,
+                        left: 30,
+                        child: Container(
+                          width: 230,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Icon(
+                                  Icons.developer_mode_outlined,
+                                  color: Colors.blue,
+                                ),
+                                Text(
+                                  "Mohith B M",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.call_made_outlined,
+                                  color: Colors.black,
+                                  size: 30,
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
                     ],
                   ),
-                ),
-                const SizedBox(width: 20),
-                FadeInAnimation(
-                    duration: Duration(milliseconds: 3000),
-                    curve: Curves.linear,
-                    child: Container(
-                      width: 700,
-                      color: Colors.black,
-                      child: Stack(
-                        children: [
-                          Center(
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 90),
-                              child: Container(
-                                width: 300,
-                                height: 400,
-                                color: Colors.amber,
-                                child: const Image(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage("Assets/3.jpg"),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Positioned(
-                            top: 200,
-                            left: 50,
-                            child: Container(
-                              width: 260,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Icon(
-                                      Icons.developer_mode_outlined,
-                                      color: Colors.blue,
-                                    ),
-                                    Text(
-                                      "Application Developer",
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                    Icon(
-                                      Icons.call_made_outlined,
-                                      color: Colors.black,
-                                      size: 30,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                          Positioned(
-                            top: 300,
-                            left: 30,
-                            child: Container(
-                              width: 230,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Icon(
-                                      Icons.developer_mode_outlined,
-                                      color: Colors.blue,
-                                    ),
-                                    Text(
-                                      "Mohith B M",
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                    Icon(
-                                      Icons.call_made_outlined,
-                                      color: Colors.black,
-                                      size: 30,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ))
-              ],
-            ),
+                ))
           ],
         ),
-
+      ],
     );
   }
 }
